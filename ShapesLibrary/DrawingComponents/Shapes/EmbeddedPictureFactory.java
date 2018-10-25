b9ea8b3de1c254a4ce84912b08b5642078276dc8
Scout Droske <scdroske@aggiemail.usu.edu>
2018-10-25 12:51:21 -0600

    working on embedded images

--- ShapesLibrary/DrawingComponents/Shapes/EmbeddedPictureFactory.java
+++ ShapesLibrary/DrawingComponents/Shapes/EmbeddedPictureFactory.java
@@ -1,5 +1,38 @@
 package Shapes;
 
-public abstract class EmbeddedPictureFactory {
+import java.util.ArrayList;
+import java.util.List;
 
+public abstract class EmbeddedPictureFactory {
+    List embeddedPictures = new ArrayList();
+
+    public void addImage(EmbeddedPictureFactory embeddedPictureFactory){
+        throw new UnsupportedOperationException();
+    }
+
+    public void removeShape(EmbeddedPictureFactory embeddedPictureFactory){
+        throw new UnsupportedOperationException();
+    }
+
+    public EmbeddedPictureFactory getChild(int i){
+        throw new UnsupportedOperationException();
+    }
+
+    //extrinsic info
+
+    public String getImage(){
+        throw new UnsupportedOperationException();
+    }
+
+    public Point getLocation(){
+        throw new UnsupportedOperationException();
+    }
+
+    public double getArea(){
+        throw new UnsupportedOperationException();
+    }
+
+    public void generateImages(){
+        throw new UnsupportedOperationException();
+    }
 }
--- ShapesLibrary/DrawingComponents/Shapes/EmbeddedPictureIntrinsic.java
+++ ShapesLibrary/DrawingComponents/Shapes/EmbeddedPictureIntrinsic.java
@@ -1,9 +1,28 @@
 package Shapes;
 
+import java.awt.BorderLayout;
+import java.awt.Image;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class EmbeddedPictureIntrinsic {
-//read in file/stream of pictures
-//check if file/stream already exists in dictionary
-//if so, start next method, else add it to the dictionary
-//if in dictionary, use its
-//send info to emb pic factory or w/e to then add the extrinsic state info (decorate)
+    public static void main(String[] argv) throws Exception {
+        //Read from an input stream
+        InputStream is = new BufferedInputStream(new FileInputStream("source.gif"));
+        Image image = ImageIO.read(is);
+
+        JFrame frame = new JFrame();
+        JLabel label = new JLabel(new ImageIcon(image));
+        frame.getContentPane().add(label, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
